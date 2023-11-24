67f45ba414de0a737339ada5cd7276fb4a1e52e7
Leo Mendoza <77596@sistemas.frc.utn.edu.ar>
2023-11-24 12:09:20 -0300

    correcion en la funcion de UV

--- src/helpers/indiceUV.js
+++ src/helpers/indiceUV.js
@@ -1,13 +1,13 @@
 function obtenerNivelUV(indiceUV) {
     if (indiceUV >= 0 && indiceUV <= 2) {
       return "Bajo";
-    } else if (indiceUV >= 3 && indiceUV <= 5) {
+    } else if (indiceUV > 2 && indiceUV <= 5) {
       return "Moderado";
-    } else if (indiceUV >= 6 && indiceUV <= 7) {
+    } else if (indiceUV > 5 && indiceUV <= 7) {
       return "Alto";
-    } else if (indiceUV >= 8 && indiceUV <= 10) {
+    } else if (indiceUV > 7 && indiceUV <= 10) {
       return "Muy alto";
-    } else if (indiceUV >= 11) {
+    } else if (indiceUV > 10) {
       return "Extremadamente alto";
     } else {
       return "Valor no válido";
